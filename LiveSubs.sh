9cb5416871fe9e95cb3034bc8cd2708c36979164
Nazir_404 <75011852+vamos404@users.noreply.github.com>
2024-10-30 18:03:55 -0400

    Update LiveSubs.sh

--- LiveSubs.sh
+++ LiveSubs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Author: Nazir Bin Nazrul
+# Description: Simple BASH script to filter out live subdomains.
+# Date creation: 31/10/2024
+# Last Modified: 31/10/2024
 
 echo
 while read subdomain; do
